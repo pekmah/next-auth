2ed31e15e20c793577d25d8087818baa53b5c3cd
Pekstaar <pekstaar@gmail.com>
2024-09-23 20:11:42 +0300

    feat: action client created globally

--- components/actions/login.ts
+++ components/actions/login.ts
@@ -1,9 +1,8 @@
 "use server";
 
 import {LoginSchema} from "@/app/schemas";
-import {createSafeActionClient} from "next-safe-action";
+import {actionClient} from "@/lib/safe-action";
 
-const actionClient = createSafeActionClient();
 
 export const loginAction = actionClient.schema(LoginSchema).action(async ({parsedInput}) => {
 
@@ -15,19 +14,3 @@ export const loginAction = actionClient.schema(LoginSchema).action(async ({parse
 
     return {success: "Email Sent"}
 });
-
-// export const login = async (values: any) => {
-//
-//     await new Promise(resolve => setTimeout(resolve, 2000));
-//     console.log("FORM DATA IS:::", values)
-//
-//     const validatedFields = LoginSchema.safeParse(values);
-//
-//     if (!validatedFields.success) {
-//         return {error: "Invalid Fields"}
-//         // throw new Error("Invalid fields");
-//     }
-//
-//     return {success: "Email Sent"}
-//
-// }
